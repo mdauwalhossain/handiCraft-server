9be68b6ac607ace110e4f27698c46a392ad2add5
Md Auwal Hossain <mdauwalhossain2020@gmail.com>
2021-11-11 23:51:23 +0600

    post

--- index.js
+++ index.js
@@ -14,12 +14,19 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 // console.log(uri)
 
-
-
 async function run() {
     try{
         await client.connect()
-        console.log('Database connected successfully');
+        const database = client.db('handcraft')
+        const purchaseCollection = database.collection('purchase')
+
+        app.post('/purchase', async(req, res) =>{
+            const appointment = req.body;
+            const result = await purchaseCollection.insertOne(appointment)
+            console.log(result);
+            res.json(result)
+
+        })
     }
     finally{
         // await client.close();
